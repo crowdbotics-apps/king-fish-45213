52c2e2ee9f2f05fafb25f2df5288bf93370f1c96
Crowdbotics <team@crowdbotics.com>
2023-12-28 07:05:46 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "king-fish-45213",
+  url: "https://king-fish-45213.botics.co",
+  api: "https://king-fish-45213.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
